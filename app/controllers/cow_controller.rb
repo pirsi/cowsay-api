06df2df5895e194f661047812a2db608c69fea21
mp <mploureno@gmail.com>
2020-07-17 18:29:26 -0300

    Added some parameters and validate

--- app/controllers/cow_controller.rb
+++ app/controllers/cow_controller.rb
@@ -1,5 +1,25 @@
 class CowController < ApplicationController
   def say
-    @message = Cow.new.say(params[:message])
+    params.require(:message)
+    params.permit(
+      :cow,
+      :balloon_type,
+      :face_type
+    )
+    
+    message      = params[:message]
+    cow          = params[:cow] || 'cow'
+    balloon_type = params[:balloon_type] || 'say'
+    face_type    = params[:face_type] || 'default'
+
+    @message = Cow
+      .new(
+        cow: cow,
+        face_type: face_type
+      )
+      .say(
+        message,
+        balloon_type
+      )
   end
 end
